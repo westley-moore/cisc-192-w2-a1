c10b94f4411133d276fa721b5f73d0a5522bdfe7
Westley <westleyhm@gmail.com>
2026-09-02 10:11:27 -0700

    inital code for the project

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float RoomL = 22.5f;
+    int RoomW = 22;
+    int Windows = 4;
+    int HasProjector = 1;
+    cout << "Room Length: " << RoomL << " Feet" << endl;
+    cout << "Room Width: " << RoomW << " Feet" << endl;
+    cout << "Number of Windows: " << Windows << endl;
+    cout << "Has Projector: " << HasProjector << endl;
+}
